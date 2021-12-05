d443234dd8614a32ea79cab153c67d59c5084dcd
Camilo <camilo.verav@sansano.usm.cl>
2021-12-05 19:30:40 -0300

    eliminada opción de semanas

--- db.py
+++ db.py
@@ -7,7 +7,9 @@ c = conn.cursor()
 c.execute("SELECT title FROM content_contentnode")
 res = c.fetchall()
 for i in range(len(res)):
-    print("["+str(i)+"]"+res[i][0])
+    if res[i][0][:6] != "Semana":
+        print("["+str(i)+"]"+res[i][0])
+
 inn = int(input("Ingrese el número del documento que desea buscar: "))
 # TODO: solucionar problema de semanas, ya que no son un recurso en sí, sino una "carpeta" de recursos
 c.execute("SELECT id FROM content_contentnode WHERE title = '" + res[inn][0]+"'")
@@ -18,5 +20,7 @@ if c.execute("SELECT local_file_id FROM content_file WHERE contentnode_id = \"" 
 if c.execute("SELECT local_file_id FROM content_file WHERE contentnode_id = \"" + str(res[0][0])+"\" AND preset LIKE '%thumbnail%'"):
     thumb = c.fetchall()[0][0]
     print("La miniatura se encuentra en storage/" +str(thumb[0]) + "/" + str(thumb[1]) + "/" + str(thumb))
+
+
 
 
