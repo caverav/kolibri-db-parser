86863621945653a4656a51055b07016e71e6c7a4
Camilo <camilo.verav@sansano.usm.cl>
2021-12-14 02:31:57 -0300

    agregados local_file_id del recurso y la miniatura al csv relacionando la tabla content_contentnode con la tabla content_file

--- kolibri-conversor-base-de-datos.py
+++ kolibri-conversor-base-de-datos.py
@@ -14,14 +14,33 @@ c = conn.cursor()
 
 # CREACION DEL CSV
 import csv
+fieldnames = ['id', 'title', 'content_id', 'channel_id', 'description', 'sort_order', 'license_owner', 'author', 'kind', 'available', 'lft', 'rght', 'tree_id', 'level', 'lang_id', 'license_description', 'license_name', 'coach_content', 'num_coach_contents', 'on_device_resources', 'options', 'parent_id', 'local_file_id', 'local_file_id_thumb']
 with open('content_contentnode.csv', 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile)
-    fieldnames = ['id', 'title', 'content_id', 'channel_id', 'description', 'sort_order', 'license_owner', 'author', 'kind', 'available', 'lft', 'rght', 'tree_id', 'level', 'lang_id', 'license_description', 'license_name', 'coach_content', 'num_coach_contents', 'on_device_resources', 'options', 'parent_id']
     csvwriter.writerow(fieldnames)
+    conn2 = sqlite3.connect(db)
+    c2 = conn2.cursor()
+    c = conn.cursor()
     for row in c.execute('SELECT * FROM content_contentnode'):
-        csvwriter.writerow(row)
+        local_file_id = c2.execute('SELECT local_file_id FROM content_file WHERE contentnode_id = "' + str(row[0]) + '" AND thumbnail = 0').fetchone()
+        local_file_id_thumb = c2.execute('SELECT local_file_id FROM content_file WHERE contentnode_id = "' + str(row[0]) + '" AND thumbnail = 1').fetchone()
+        print("Row: ")
+        print(row)
+        print("Local_file_id: ")
+        if local_file_id is None:
+            print("None")
+        else:
+            print(local_file_id[0])
+            local_file_id = local_file_id[0]
+        if local_file_id_thumb is None:
+            print("None")
+        else:
+            print(local_file_id_thumb[0])
+            local_file_id_thumb = local_file_id_thumb[0]
+        csvwriter.writerow(tuple(list(row)+[local_file_id]+[local_file_id_thumb]))
 
 conn.close()
+conn2.close()
 csvfile.close()
 
 # c.execute("SELECT id FROM content_contentnode WHERE title = '" + res[inn][0]+"'")
